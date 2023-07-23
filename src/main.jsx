681c6b5608fb31f54c2b1214dd389be9aa24a02f
mahdibashirione <mahdi94250@gmail.com>
2023-07-23 17:25:24 +0330

    bug fix

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,10 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-import Layout from "./Layout/Layout";
 import { Provider } from "react-redux";
 import store from "./feature/store.js";
 import { Toaster } from "react-hot-toast";
+import Layout from "./Layout/Layout.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
